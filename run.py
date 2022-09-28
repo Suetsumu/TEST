27bdfcbc9e7b54d3046ae2f08ca9b33ebb62007b
Suetsumu <miunadaisuki123@gmail.com>
2022-09-28 19:47:18 +0800

    parallel

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import argparse
+
 import torch
 import torchvision
 import numpy
@@ -12,42 +14,45 @@ from model.resnet20 import ResNet20
 from dataset_prepare.cifar10 import read_cifar10
 import os
 os.environ['CUDA_VISIBLE_DEVICES']='1,2'
-
-
-
-def main():
+import torch.multiprocessing as mp
+import torch.distributed as dist
+import torchvision.transforms as transforms
+import datetime
+def deparallel_train():
     since = time.time()
     data_dir = './dataset/cifar'
     model_dir = './cnn'
     log_dir = './log'
     batchsize = 128
     n_epochs = 200
-    best_acc = 0.0#记录测试最高准确率
+    best_acc = 0.0  # 记录测试最高准确率
     Lr = 0.1
 
-    data_loader_train,data_loader_test = read_cifar10(batchsize,data_dir)
+    data_loader_train, data_loader_test = read_cifar10(batchsize, data_dir)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = ResNet20().to(device)
     print(model)
-    #取出权重参数和偏置参数，仅对权重参数加惩罚系数
-    weight_p, bias_p = [],[]
+    # 取出权重参数和偏置参数，仅对权重参数加惩罚系数
+    weight_p, bias_p = [], []
     for name, p in model.named_parameters():
         if 'bias' in name:
-            bias_p +=[p]
+            bias_p += [p]
         else:
-            weight_p +=[p]
-    cost = nn.CrossEntropyLoss().to(device)      #Q3：要不要加正则项
+            weight_p += [p]
+    cost = nn.CrossEntropyLoss().to(device)  # Q3：要不要加正则项
 
-    #L1正则
+    # L1正则
     # regularization_loss = 0
     # for param in model.parameters():
     #     regularization_loss += torch.sum(torch.abs(param))
     #
     # optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
-    optimizer = torch.optim.SGD([{'params':weight_p,'weight_decay':1e-4},
-                               {'params':bias_p,'weight_decay':0}],lr=Lr,momentum=0.9)#内置的SGD是L2正则，且对所有参数添加惩罚，对偏置加正则易导致欠拟合，一般只对权重正则化
-    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer,milestones=[82,122,163],gamma=0.1,last_epoch=-1)#Q4: 对应多少步,epoch= 32000/(50000/batch_size),48000,64000
+    optimizer = torch.optim.SGD([{'params': weight_p, 'weight_decay': 1e-4},
+                                 {'params': bias_p, 'weight_decay': 0}], lr=Lr,
+                                momentum=0.9)  # 内置的SGD是L2正则，且对所有参数添加惩罚，对偏置加正则易导致欠拟合，一般只对权重正则化
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[82, 122, 163], gamma=0.1,
+                                                     last_epoch=-1)  # Q4: 对应多少步,epoch= 32000/(50000/batch_size),48000,64000
 
     Loss_list = []
     Accuracy_list = []
@@ -58,40 +63,40 @@ def main():
         training_loss = 0.0
         training_correct = 0
         training_acc = 0.0
-        print("Epoch {}/{}".format(epoch+1,n_epochs))
-        print("-"*30)
+        print("Epoch {}/{}".format(epoch + 1, n_epochs))
+        print("-" * 30)
 
         total_train = 0
-        for i,data in enumerate(data_loader_train):
-            x,labels = data
-            x,labels = x.to(device), labels.to(device)
+        for i, data in enumerate(data_loader_train):
+            x, labels = data
+            x, labels = x.to(device), labels.to(device)
 
             # print(x.shape)
             # print(label.shape)
-            #前向传播计算损失
+            # 前向传播计算损失
             outputs = model(x)
             loss = cost(outputs, labels)
             training_loss += loss.item()
             # print(outputs)
-            _,pred = torch.max(outputs,1)#预测最大值所在位置标签
+            _, pred = torch.max(outputs, 1)  # 预测最大值所在位置标签
             total_train += labels.size(0)
             num_correct = (pred == labels).sum()
             training_acc += num_correct.item()
 
-            #反向传播+优化
+            # 反向传播+优化
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-        # print('Epoch：', epoch, 'train loss:', training_loss/len(data_loader_train))
-        # Loss_list.append(training_loss/len(data_loader_train))
+            # print('Epoch：', epoch, 'train loss:', training_loss/len(data_loader_train))
+            # Loss_list.append(training_loss/len(data_loader_train))
             if i % 100 == 99:
                 print('[%d, %5d] traing_loss: %f' % (epoch + 1, i + 1, training_loss / 100))
                 logger.info('[%d, %5d] traing_loss: %f' % (epoch + 1, i + 1, training_loss / 100))
                 Loss_list.append(training_loss / 100)
                 training_loss = 0.0
-        print('Train acc:{:.4}%'.format(100*training_acc/total_train))
-        logger.info('Train acc:{:.4}%'.format(100*training_acc/total_train))
+        print('Train acc:{:.4}%'.format(100 * training_acc / total_train))
+        logger.info('Train acc:{:.4}%'.format(100 * training_acc / total_train))
 
         scheduler.step()
 
@@ -103,20 +108,20 @@ def main():
                 x_test, label_test = data
                 x_test, label_test = x_test.to(device), label_test.to(device)
                 outputs = model(x_test)
-                _,pred = torch.max(outputs.data,1)
+                _, pred = torch.max(outputs.data, 1)
                 total += label_test.size(0)
                 testing_correct += (pred == label_test).sum().item()
-        print('Test acc: {:.4}%'.format(100*testing_correct/total))
-        logger.info('Test acc: {:.4}%'.format(100*testing_correct/total))
-        Accuracy_list.append(100*testing_correct/total)
-        acc = 100*testing_correct/total
-        if acc>best_acc:
+        print('Test acc: {:.4}%'.format(100 * testing_correct / total))
+        logger.info('Test acc: {:.4}%'.format(100 * testing_correct / total))
+        Accuracy_list.append(100 * testing_correct / total)
+        acc = 100 * testing_correct / total
+        if acc > best_acc:
             best_acc = acc
             best_acc_loc = epoch
         # print("Loss :{:.4f}, Train acc :{.4f}, Test acc :{.4f}".format(training_loss/len(data_train),100*training_correct/len(data_train),100*testing_correct/len(data_test)))
 
-    print('test best acc:{}% at epoch{}'.format(best_acc,best_acc_loc))
-    logger.info('test best acc:{}% at epoch{}'.format(best_acc,best_acc_loc))
+    print('test best acc:{}% at epoch{}'.format(best_acc, best_acc_loc))
+    logger.info('test best acc:{}% at epoch{}'.format(best_acc, best_acc_loc))
     time_used = time.time() - since
     print('-' * 30)
     print('训练用时： {:.0f}m {:.0f}s'.format(time_used // 60, time_used % 60))
@@ -124,10 +129,118 @@ def main():
     # print('最高准确率: {}%'.format(100 * best_acc))
     logger.info('最高准确率: {}%'.format(100 * best_acc))
 
-    #保存参数优化器等
+    # 保存参数优化器等
     state = {'model': model.state_dict(), 'optimizer': optimizer.state_dict(), 'epoch': epoch}
-    torch.save(state, os.path.join(model_dir,'{}best_acc.pth'.format(best_acc)))
+    torch.save(state, os.path.join(model_dir, '{}best_acc.pth'.format(best_acc)))
+
+def parallel_train(gpu,args):
+    ############################################################
+    rank = args.nr * args.gpus + gpu
+    dist.init_process_group( #初始化分布式环境
+        backend='nccl',   #各个进程的通信协议
+        init_method='env://', #参数化方法
+        world_size=args.world_size, #环境变量的配置
+        rank=rank
+    )
+    ############################################################
+    #这里设置了种子，定义了模型，设置了gpu，数据集块，交叉熵loss，SGD优化器
+    torch.manual_seed(0)
+    model = ResNet20()
+    torch.cuda.set_device(gpu)
+    model.cuda(gpu)
+    batch_size = 100
+    # define loss function (criterion) and optimizer
+    criterion = nn.CrossEntropyLoss().cuda(gpu)
+    optimizer = torch.optim.SGD(model.parameters(), 1e-4)
+
+    ###############################################################
+    # Wrap the model 通过DistributedDataParallel包装模型
+    model = nn.parallel.DistributedDataParallel(model,
+                                                device_ids=[gpu])
+    ###############################################################
+
+    # Data loading code
+    train_dataset = torchvision.datasets.MNIST(
+        root='./dataset/mnist',
+        train=True,
+        transform=transforms.ToTensor(),
+        download=True
+    )
+    ################################################################
+    #通过DistributedSampler采样数据
+    train_sampler = torch.utils.data.distributed.DistributedSampler(
+        train_dataset,
+        num_replicas=args.world_size,
+        rank=rank
+    )
+    ################################################################
+
+    train_loader = torch.utils.data.DataLoader(
+        dataset=train_dataset,
+        batch_size=batch_size,
+        ##############################
+        shuffle=False,  #
+        ##############################
+        num_workers=0,
+        pin_memory=True,
+        #############################
+        sampler=train_sampler)  #
+    #############################
+
+    start = datetime.now()
+    total_step = len(train_loader)
+    for epoch in range(args.epochs):
+        for i, (images, labels) in enumerate(train_loader):
+            images = images.cuda(non_blocking=True)
+            labels = labels.cuda(non_blocking=True)
+            # Forward pass
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            if (i + 1) % 100 == 0 and gpu == 0:
+                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(
+                    epoch + 1,
+                    args.epochs,
+                    i + 1,
+                    total_step,
+                    loss.item())
+                   )
+                logger.info('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(
+                    epoch + 1,
+                    args.epochs,
+                    i + 1,
+                    total_step,
+                    loss.item())
+                   )
+    if gpu == 0:
+        print("Training complete in: " + str(datetime.now() - start))
+        logger.info("Training complete in: " + str(datetime.now() - start))
+
+def main():
+    #导入argparse方便使用sh脚本传值
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--nodes', default=1,
+                        type=int, metavar='N') #节点数
+    parser.add_argument('-g', '--gpus', default=1, type=int,
+                        help='number of gpus per node')
+    parser.add_argument('-nr', '--nr', default=0, type=int,
+                        help='ranking within the nodes') #node的序号
+    parser.add_argument('--epochs', default=2, type=int,
+                        metavar='N',
+                        help='number of total epochs to run')  #总训练轮数
+    args = parser.parse_args()
+    #########################################################
+    args.world_size = args.gpus * args.nodes  #
+    os.environ['MASTER_ADDR'] = '119.23.152.46'  #主进程的IP
+    os.environ['MASTER_PORT'] = '8888'  #
+    mp.spawn(parallel_train, nprocs=args.gpus, args=(args,))  #
+    #########################################################
+
 
 if __name__ == '__main__':
 
-    main()
+    deparallel_train()
